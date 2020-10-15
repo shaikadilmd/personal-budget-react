754949bffb6a8d617970fdd915dc2670390dfff6
shaikadilmd <shaikadilmd@gmail.com>
2020-10-15 02:43:01 -0400

    Brought PB-Code

---
+++ src/AboutPage/AboutPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+function AboutPage() {
+  return (
+    <div>
+        AboutPage   
+    </div>
+  );
+}
+
+export default AboutPage;
+
+
---
+++ src/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+function Footer() {
+  return (
+    <footer className="bottom">
+    <div className="center">
+        All rights reserved &copy; Shaik Adil
+    </div>
+    </footer>
+  );
+}
+
+export default Footer;
